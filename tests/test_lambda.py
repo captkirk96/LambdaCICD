1991ef889198d14721ffdcd40a84140b335325f0
mathewchris96 <83598643+captkirk96@users.noreply.github.com>
2025-01-28 15:02:14 +0530

    Update test_lambda.py

--- tests/test_lambda.py
+++ tests/test_lambda.py
@@ -1,5 +1,6 @@
 import sys
 import os
+print(f"Current working directory: {os.getcwd()}")
 
 # Add the root directory to the Python path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
